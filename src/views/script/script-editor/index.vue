0c218d7490f3707548d09f79d24f21972959f063
Kelvin.Ye <232647843@qq.com>
2020-03-24 09:46:42 +0800

    update

--- src/views/script/script-editor/index.vue
+++ src/views/script/script-editor/index.vue
@@ -1,7 +1,34 @@
 <template>
   <scrollbar class="app-main-container">
     <div class="script-editor-container">
-      {{ itemNo }}
+      <div class="element-sidebar-container">
+        <el-input
+          v-model="searchKeyword"
+          placeholder="测试集合 | 测试案例 | 测试步骤"
+          prefix-icon="el-icon-search"
+          @keyup.enter.native="handleEnterSearch"
+        />
+        <el-tabs v-model="elementSidebarTabActiveName" :stretch="true" @tab-click="handleElementSidebarTabClick">
+          <el-tab-pane label="测试集合" name="collection">
+            <div class="collection-operation-button-container">
+              <el-button type="text" icon="el-icon-plus">新增</el-button>
+              <el-divider direction="vertical" />
+              <el-button type="text" icon="el-icon-delete">删除</el-button>
+              <el-divider direction="vertical" />
+              <el-button type="text" icon="el-icon-copy-document">复制</el-button>
+              <el-divider direction="vertical" />
+              <el-button type="text" icon="el-icon-bottom-left">粘贴</el-button>
+            </div>
+            <el-divider />
+          </el-tab-pane>
+          <el-tab-pane label="测试案例" name="group">测试案例</el-tab-pane>
+          <el-tab-pane label="测试步骤" name="sampler">测试步骤</el-tab-pane>
+        </el-tabs>
+      </div>
+      <div class="editor-main-container">
+        <p>searchKeyword: {{ searchKeyword }}</p>
+        <p>elementSidebarTabActiveName: {{ elementSidebarTabActiveName }}</p>
+      </div>
     </div>
   </scrollbar>
 </template>
@@ -12,12 +39,23 @@ export default {
 
   data() {
     return {
-      itemNo: ''
+      itemNo: '',
+      searchKeyword: '',
+      elementSidebarTabActiveName: 'collection'
     }
   },
 
   mounted: function() {
     this.itemNo = this.$route.params.itemNo
+  },
+
+  methods: {
+    handleEnterSearch(event, keyword) {
+      console.log(keyword)
+    },
+    handleElementSidebarTabClick(tab, event) {
+      console.log(tab, event)
+    }
   }
 }
 </script>
@@ -26,16 +64,48 @@ export default {
   .script-editor-container {
     display: flex;
     flex: 1;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: space-between;
 
     height: 100%;
     padding: 20px;
 
-    background-color: #008489;
-
     .el-divider--horizontal {
       margin: 10px 0
     }
+  }
+
+  .element-sidebar-container{
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    max-width: 400px;
+    padding: 12px;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+
+  }
+
+  .collection-operation-button-container{
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+    .el-button--text {
+        padding-top: 6px;
+        padding-bottom: 6px;
+    }
+  }
+
+  .editor-main-container{
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+    margin-left: 12px;
+    background-color:#5a5e66;
   }
 </style>
