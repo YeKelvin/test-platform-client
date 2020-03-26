6baa3f2927f63347a73ef093a002133b8dd27b93
Kelvin.Ye <232647843@qq.com>
2020-03-26 16:51:19 +0800

    添加防止重复提交功能

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,37 +1,83 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import { MessageBox, Message, Loading } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+import _ from 'lodash'
 
-// create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API,
-  timeout: 5000,
+  baseURL: process.env.VUE_APP_BASE_API, // 请求的 base url
+  timeout: 5000, // 超时时长
   headers: {
     'Content-Type': 'application/json; charset=utf-8'
   }
 })
 
-// request interceptor
+// loading对象
+let loading
+
+// 当前正在请求的数量
+let needLoadingRequestCount = 0
+
+// 显示loading
+function showLoading() {
+  // 后面这个判断很重要，因为关闭时加了抖动，此时 loading对象可能还存在，
+  // 但 needLoadingRequestCount已经变成0，避免这种情况下会重新创建个 loading
+  if (needLoadingRequestCount === 0 && !loading) {
+    loading = Loading.service({
+      lock: true,
+      text: 'Loading',
+      background: 'rgba(0, 0, 0, 0.5)'
+    })
+  }
+  needLoadingRequestCount++
+}
+
+// 隐藏loading
+function hideLoading() {
+  needLoadingRequestCount--
+  needLoadingRequestCount = Math.max(needLoadingRequestCount, 0) // 做个保护
+  if (needLoadingRequestCount === 0) {
+    // 关闭loading
+    toHideLoading()
+  }
+}
+
+// 防抖：将 300ms 间隔内的关闭 loading 便合并为一次。防止连续请求时， loading闪烁的问题。
+const toHideLoading = _.debounce(() => {
+  loading.close()
+  loading = null
+}, 300)
+
+// 请求拦截器
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
     if (store.getters.token) {
       config.headers['Authorization'] = 'JWT ' + getToken()
+    }
+    // 判断当前请求是否设置了不显示Loading
+    if (config.method !== 'get') {
+      showLoading()
     }
     return config
   },
   error => {
-    // do something with request error
+    // 判断当前请求是否设置了不显示Loading
+    if (error.config.method !== 'get') {
+      hideLoading()
+    }
     Message({ message: error.message, type: 'error', duration: 5 * 1000 })
     return Promise.reject(error)
   }
 )
 
-// response interceptor
+// 响应拦截器
 service.interceptors.response.use(
   response => {
     const { data } = response
+    // 判断当前请求是否设置了不显示Loading（不显示自然无需隐藏）
+    if (response.config.method !== 'get') {
+      hideLoading()
+    }
     if (response.status !== 200) {
       Message({ message: response.statusText || 'Error!', type: 'error', duration: 5 * 1000 })
       return Promise.reject(new Error(response.statusText || 'Error!'))
@@ -56,7 +102,10 @@ service.interceptors.response.use(
     }
   },
   error => {
-    // do something with request error
+    // 判断当前请求是否设置了不显示Loading（不显示自然无需隐藏）
+    if (error.config.method !== 'get') {
+      hideLoading()
+    }
     Message({ message: error.message, type: 'error', duration: 5 * 1000 })
     return Promise.reject(error)
   }
@@ -64,5 +113,4 @@ service.interceptors.response.use(
 
 export default service
 
-// todo axios防抖处理
 // todo 页面转跳时取消所有未返回的请求
