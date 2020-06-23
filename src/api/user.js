eb077313cd834ab30129f39cb008049f4cfc4d49
Kelvin.Ye <testmankelvin@163.com>
2020-06-24 00:05:48 +0800

    update

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function encryptionFactor(query) {
   return request({
-    url: '/encryption/factor',
+    url: '/user/encryption/factor',
     method: 'get',
     params: query
   })
