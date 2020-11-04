77023028d6142708c154a68c11ac17bcdb50448c
Kelvin.Ye <testmankelvin@163.com>
2020-11-04 23:55:35 +0800

    update

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,24 +1,29 @@
 <template>
   <scrollbar class="app-main-container">
     <div class="user-management-container">
-
       <div class="query-conditions-container">
         <div>查询条件</div>
         <el-divider />
         <div class="condition-items">
-          <condition-input v-model="queryConditions.userNo" label="用户编号" class="condition-item" />
-          <condition-input v-model="queryConditions.userName" label="用户名称" class="condition-item" />
-          <condition-input v-model="queryConditions.mobileNo" label="手机号" class="condition-item" />
-          <condition-input v-model="queryConditions.email" label="邮箱地址" class="condition-item" />
-          <condition-select v-model="queryConditions.state" :options="UserState" label="用户状态" class="condition-item" />
+          <condition-input v-model="queryConditions.userNo" size="small" label="用户编号" class="condition-item" />
+          <condition-input v-model="queryConditions.userName" size="small" label="用户名称" class="condition-item" />
+          <condition-input v-model="queryConditions.mobileNo" size="small" label="手机号" class="condition-item" />
+          <condition-input v-model="queryConditions.email" size="small" label="邮箱地址" class="condition-item" />
+          <condition-select
+            v-model="queryConditions.state"
+            :options="UserState"
+            label="用户状态"
+            class="condition-item"
+            size="small"
+          />
         </div>
         <div class="query-buttons-container">
           <div />
           <div class="query-buttons">
-            <el-button type="primary" @click="query">查询</el-button>
-            <el-button @click="resetQueryConditions">重置</el-button>
+            <el-button type="primary" size="small" @click="query">查询</el-button>
+            <el-button size="small" @click="resetQueryConditions">重置</el-button>
           </div>
-          <el-button type="primary" @click="registerDialogVisible=true">新增</el-button>
+          <el-button type="primary" size="small" @click="registerDialogVisible=true">新增</el-button>
         </div>
       </div>
 
@@ -113,7 +118,8 @@ export default {
         const { result } = response
         this.tableData = result['dataSet']
         this.totalSize = result['totalSize']
-      }).catch(() => {})
+      }).catch(() => {
+      })
     },
     resetQueryConditions() {
       Object.keys(this.queryConditions).forEach(key => {
@@ -139,8 +145,10 @@ export default {
             // 重新查询列表
             this.query()
           }
-        }).catch(() => {})
-      }).catch(() => {})
+        }).catch(() => {
+        })
+      }).catch(() => {
+      })
     },
     resetPassword(row) {
       this.$confirm(
@@ -150,8 +158,10 @@ export default {
           if (response.success) {
             this.$message({ message: '重置用户密码成功', type: 'info', duration: 2 * 1000 })
           }
-        }).catch(() => {})
-      }).catch(() => {})
+        }).catch(() => {
+        })
+      }).catch(() => {
+      })
     },
     deleteUser(row) {
       this.$confirm(
@@ -163,8 +173,10 @@ export default {
             // 重新查询列表
             this.query()
           }
-        }).catch(() => {})
-      }).catch(() => {})
+        }).catch(() => {
+        })
+      }).catch(() => {
+      })
     },
     openModifyDialog(row) {
       this.modifyDialogVisible = true
@@ -175,70 +187,70 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .user-management-container {
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    justify-content: space-between;
+.user-management-container {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
 
-    height: 100%;
-    padding: 20px;
+  height: 100%;
+  padding: 20px;
 
-    .el-divider--horizontal {
-      margin: 10px 0
-    }
+  .el-divider--horizontal {
+    margin: 10px 0
   }
+}
 
-  .query-conditions-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+.query-conditions-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 
-    padding: 12px;
-    border-radius: 4px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-  }
+  padding: 12px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+}
 
-  .condition-items {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    flex: none;
-  }
+.condition-items {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  flex: none;
+}
 
-  .condition-item {
-    display: inline-flex;
-    justify-content: flex-start;
-    align-items: center;
-    color: #909399;
-    font-size: 14px;
-    width: 20rem;
-    padding-right: 24px;
-    padding-bottom: 12px;
-  }
+.condition-item {
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  color: #909399;
+  font-size: 14px;
+  width: 20rem;
+  padding-right: 24px;
+  padding-bottom: 12px;
+}
 
-  .query-buttons-container {
-    display: flex;
-    justify-content: space-between;
-  }
+.query-buttons-container {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .query-buttons {
-    display: flex;
-    justify-content: center;
-  }
+.query-buttons {
+  display: flex;
+  justify-content: center;
+}
 
-  .table-container {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
+.table-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 
-    padding: 12px;
-    border-radius: 4px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-  }
+  padding: 12px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+}
 
-  .pagination-container {
-    padding-top: 12px;
-    padding-bottom: 12px;
-  }
+.pagination-container {
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
 </style>
